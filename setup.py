e14c15237e55e447113637d6fe24120608109ae6
Yigit Ozen <canyigitozen@gmail.com>
2014-06-13 12:07:01 +0300

    Added manifest.in

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from distutils.core import setup
 
 
 setup(name='django-tagging',
-      version='1.0',
+      version='1.0.1',
       description='Multilingual tagging system for Django',
       author='Yigit Ozen',
+      license='MIT',
       packages=['tagging',],
       requires=['django', 'unicodecsv']
 )
