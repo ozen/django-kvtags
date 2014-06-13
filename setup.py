4d5786a1744f8bdcc803c6e3e41d82d02932799d
Yigit Ozen <canyigitozen@gmail.com>
2014-06-13 12:42:44 +0300

    Fixed setup.py, added setuptools

--- setup.py
+++ setup.py
@@ -1,11 +1,29 @@
-from distutils.core import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
 
 
-setup(name='django-tagging',
-      version='1.0.1',
-      description='Multilingual tagging system for Django',
-      author='Yigit Ozen',
-      license='MIT',
-      packages=['tagging',],
-      requires=['django', 'unicodecsv']
+setup(
+    name='django-tagging',
+    version='1.0.2',
+    description='Multilingual tagging system for Django',
+    author='Yigit Ozen',
+    license='MIT',
+    packages=[
+        'tagging',
+        'tagging.management',
+        'tagging.management.commands',
+        'tagging.migrations',
+        ],
+    requires=[
+        'Django>=1.5',
+        'unicodecsv>=0.9.4'
+    ],
+    package_data={'tagging': ['templates/*']},
 )
+
